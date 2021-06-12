92d8f546e373f54159bbe8039389ff4839765e21
Mario Felippe <mfchaves@live.com>
2021-06-12 14:28:32 -0300

    Operação básica de consulta de URL.

--- app/controllers/terminal_controlle.py
+++ app/controllers/terminal_controlle.py
@@ -1,10 +1,44 @@
 from app.views import terminal
+from app.classes.crawler import Crawler
 
 class TerminalController():
     
     def init(self):
-        op = terminal.menu_princial()
-        print(op)
+        
+        crawler = Crawler()
+        url = None
+        while True:
+            
+            op = terminal.menu_princial(url)
+            if op == '99':
+                terminal.mensagem('Fechando o programa...')
+                terminal.limpar_tela()
+                break
+            
+            if op == '1':
+                retorno = terminal.opcao_1()
+                if retorno == '99':
+                    terminal.mensagem('Fechando o programa...')
+                    terminal.limpar_tela()
+                    break
+                else:
+                    crawler.get_page(retorno)
+                    urls = crawler.get_urls()
+                    url = retorno
+            
+            if op == '2':
+                if not url:
+                    retorno = terminal.opcao_1()
+                    if retorno == '99':
+                        terminal.mensagem('Fechando o programa...')
+                        terminal.limpar_tela()
+                        break  
+                    else:
+                        crawler.get_page(retorno)
+                        urls = crawler.get_urls()
+                        url = retorno                   
+                else:
+                     terminal.exibir_urls(urls)
+                    
+           
 
-        op = f'terminal.opcao_{op}()'
-        exec(op)
--- app/views/terminal.py
+++ app/views/terminal.py
@@ -1,28 +1,61 @@
 import os
+import time
 
-def menu_princial():
-    
-    # Limpa a tela do terminal.
-    
+def header(url):
+    limpar_tela()
+    if not url:
+        url = ''
+    print(f'''
+          ###########################################################################
+                                           WebCrawler                    {url}
+          ###########################################################################
+          ''')
+   
+def lista_opcoes(opcoes):    
+      
+    for op in opcoes:
+        print(f'                                {op}')
+  
+def mensagem(msg):
     os.system('cls' if os.name == 'nt' else 'clear')
+    header('')
+    print(f'''          
+                                    {msg}
+          
+          ''')
+    input_data('Aperte para continuar...')
     
-    print('############################################')
-    print('                WebCrawler                  ')
-    print('############################################')
 
-    print('1 - Obter informações da página.')
-    print('99 - Sair.\n')
-    op = input('Informe a opção: ')
+def input_data(msg):
+    
+    entrada = input(f'\n                                {msg} ')
+    return entrada
+
+def menu_princial(url):
+    
+  
+    header(url)
+    opcoes = ['1 - Obter informações da URL.','2 - Listar todas os links.','99 - sair']    
+    lista_opcoes(opcoes)
+    
+    op = input('''\n          Digite a opção: ''')
     return op
+
 
 def opcao_1():
     
-    # Limpa a tela do terminal.
+    header('')
+    lista_opcoes(['99 - Sair.'])
+    url = input_data('Informe a URL:')
+    return url
+
+
+def exibir_urls(urls):
+    header('')
+    for url in urls:
+        print(f'            {url}')
+    input_data('Aperte para continuar...')
+    
+def limpar_tela():
     os.system('cls' if os.name == 'nt' else 'clear')
     
-    print('############################################')
-    print('                WebCrawler                  ')
-    print('############################################')
-
-    url = input('Informe a URL: ')
-    return url
