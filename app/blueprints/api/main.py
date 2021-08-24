aa0ff8f66f91c9f621ff7174c3a488f1d263062c
Mario Felippe <mfchaves@live.com>
2021-08-23 22:43:57 -0300

    Consulta info URLs API e consultas URLs da página.

--- app/blueprints/api/main.py
+++ app/blueprints/api/main.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, jsonify, request
 from app.classes.crawler import Crawler
+
 
 bp = Blueprint('api', __name__, url_prefix="/api/")
 
@@ -7,22 +8,60 @@ cr = Crawler()
 
 @bp.route('/')
 def index():
-    return '{"api": "minha api"}'
-
+    response = {'Consulta informação da url': '/info/(url)','Retornar as URLs da página': '/links/(url)', 'Retornar as URLs detalhadas':'/links/(url)/text'}
+    status = 200
+    return jsonify(response), status
 
 @bp.route('/info/')
-def info():
-    q = request.args.get('q')
+@bp.route('/info/<url>')
+def info(url=None):
     
-    cr.get_page(q)
-    info = cr.get_info()
-    data = {'url_request':q, 'data': info}
-   
-    return data
-
+    
+    if url:
+        if cr.get_page(url):
+            info = cr.get_info()
+            response = {'url_request': url, 'info': info}
+            status = 200
+        else:
+            response = 'A URL {} informada é inválida!'.format(q)
+            status = 400
+    else:
+        response = {'Consulta informação da url':'/info/(url)'}
+        status = 200
+        
+    return jsonify(response), status
 
 @bp.route('/links/')
-def links():
-    q = request.args.get('q')
-   
-    return '{}'.format(q)
+@bp.route('/links/<url>')
+@bp.route('/links/<url>/<text>')
+def links(url=None, text=False):
+    print(url, text)
+    
+    if text:
+        text = text.lower()
+    if text == 'true' or text == 'text':
+        text = True
+    else:
+        text = False
+    if url:
+        if cr.get_page(url):
+            info = cr.get_info()
+            links = cr.get_urls(text=text)
+            response = {'url_request':url, 'info': info, 'links': links}
+            status = 200
+        else:
+            response = 'A URL {} informada é inválida!'.format(url)
+            status = 400
+    else:
+        response = {'Retornar as URLs da página': '/links/(url)', 'Retornar as URLs detalhadas':'/links/(url)/text'}
+        status = 200
+    return jsonify(response), status
+
+
+
+@bp.route('/images/<url>')
+def images(url):
+    
+    response = {'ola':'mundo'}
+    status = 200
+    return jsonify(response), status
